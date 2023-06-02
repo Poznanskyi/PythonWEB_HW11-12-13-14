c0587293ec1929b2824ed7d43bd55e3d4ef42131
Poznanskyi <shazampoz@gmail.com>
2023-06-02 15:42:17 +0300

    in process

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -6,6 +6,14 @@ from src.schemas import ContactModel
 
 
 async def validate_birthday(brth: date):
+    """
+    The validate_birthday function takes a date object as an argument and returns the number of days until that birthday.
+        If the birthday has already passed this year, it will return the number of days until next year's birthday.
+
+    :param brth: date: Pass the user's birthday into the function
+    :return: The number of days until the user's birthday
+    :doc-author: Poznanskyi
+    """
     bth_now = date(datetime.now().year, datetime.now().month, datetime.now().day)
     brth = date(
         bth_now.year if bth_now.month < brth.month else bth_now.year + 1 if bth_now.day < brth.day else bth_now.year,
@@ -15,10 +23,24 @@ async def validate_birthday(brth: date):
 
 
 async def get_contacts(db: Session):
+    """
+    The get_contacts function returns a list of all contacts in the database.
+
+    :param db: Session: Pass the database session to the function
+    :return: A list of contact objects
+    :doc-author: Poznanskyi
+    """
     return db.query(Contact).all()
 
 
 async def get_contacts_by_birthdays(db: Session):
+    """
+        The get_contacts_by_birthdays function returns a list of contacts whose birthdays are within the next 7 days.
+
+        :param db: Session: Pass the database session to the function
+        :return: A list of contacts whose birthday is within the next 7 days
+        :doc-author: Poznanskyi
+        """
     contacts = db.query(Contact).all()
     contacts_list = []
     for contact in contacts:
@@ -28,22 +50,66 @@ async def get_contacts_by_birthdays(db: Session):
 
 
 async def get_contact_by_id(contact_id: int, db: Session):
+    """
+    The get_contact_by_id function returns a contact from the database by its id.
+
+    :param contact_id: int: Specify the id of the contact to be returned
+    :param db: Session: Pass the database session to the function
+    :return: A contact object
+    :doc-author: Poznanskyi
+    """
     return db.query(Contact).filter_by(id = contact_id).first()
 
 
 async def get_contact_by_email(email, db: Session):
+    """
+    The get_contact_by_email function returns a contact object from the database based on the email address provided.
+        Args:
+            email (str): The email address of the contact to be retrieved.
+            db (Session, optional): SQLAlchemy Session instance. Defaults to None.
+
+    :param email: Filter the query
+    :param db: Session: Pass the database session to the function
+    :return: The first contact found by the email address
+    :doc-author: Poznanskyi
+    """
     return db.query(Contact).filter_by(email=email).first()
 
 
 async def get_contact_by_name(contact_name, db: Session):
+    """
+    The get_contact_by_name function returns a list of contacts that match the contact_name parameter.
+
+
+    :param contact_name: Filter the database query
+    :param db: Session: Pass the database session to the function
+    :return: All contacts with the name specified in the contact_name parameter
+    :doc-author: Poznanskyi
+    """
     return db.query(Contact).filter_by(name=contact_name).all()
 
 
 async def get_contact_by_surname(contact_surname, db: Session):
+    """
+    The get_contact_by_surname function returns a list of contacts with the given surname.
+
+    :param contact_surname: Filter the query
+    :param db: Session: Pass in the database session to the function
+    :return: A list of contacts with the given surname
+    :doc-author: Poznanskyi
+    """
     return db.query(Contact).filter_by(surname=contact_surname).all()
 
 
 async def create_contact(body: ContactModel, db: Session):
+    """
+    The create_contact function creates a new contact in the database.
+
+    :param body: ContactModel: Pass the data from the request body to the function
+    :param db: Session: Pass the database session to the function
+    :return: A contact object
+    :doc-author: Poznanskyi
+    """
     contact = Contact(**body.dict())
     db.add(contact)
     db.commit()
@@ -51,6 +117,18 @@ async def create_contact(body: ContactModel, db: Session):
 
 
 async def update_contact(body: ContactModel, contact_id, db: Session):
+    """
+    The update_contact function updates a contact in the database.
+        Args:
+            body (ContactModel): The updated contact information.
+            db (Session): A connection to the database.
+
+    :param body: ContactModel: Get the data from the request body
+    :param contact_id: Get the contact from the database
+    :param db: Session: Pass the database session to the function
+    :return: A contact
+    :doc-author: Poznanskyi
+    """
     contact = await get_contact_by_id(contact_id, db)
     if contact:
         contact.name = body.name
@@ -64,6 +142,17 @@ async def update_contact(body: ContactModel, contact_id, db: Session):
 
 
 async def remove_contact(contact_id: int, db: Session):
+    """
+    The remove_contact function removes a contact from the database.
+        Args:
+            contact_id (int): The id of the contact to be removed.
+            db (Session): A connection to the database.
+
+    :param contact_id: Find the contact in the database
+    :param db: Session: Pass in the database session to the function
+    :return: The contact that was deleted
+    :doc-author: Poznanskyi
+    """
     contact = await get_contact_by_id(contact_id, db)
     if contact:
         db.delete(contact)
--- src/repository/users.py
+++ src/repository/users.py
@@ -6,10 +6,30 @@ from src.schemas import UserModel
 
 
 async def get_user_by_email(email: str, db: Session) -> User | None:
+    """
+    Get user by e-mail.
+
+    :param email: User's e-mail.
+    :type email: str
+    :param db: Database session.
+    :type db: Session
+    :return: Return the found user, or None if it does not exist
+    :rtype: User | None
+    """
     return db.query(User).filter(User.email == email).first()
 
 
 async def create_user(body: UserModel, db: Session) -> User:
+    """
+    Create user.
+
+    :param body: User body.
+    :type body: UserModel
+    :param db: Database session.
+    :type db: Session
+    :return: Created user.
+    :rtype: User
+    """
     avatar = None
     try:
         g = Gravatar(body.email)
@@ -24,17 +44,50 @@ async def create_user(body: UserModel, db: Session) -> User:
 
 
 async def update_token(user: User, token: str | None, db: Session) -> None:
+    """
+    Update user token
+
+    :param user: Current user.
+    :type user: User
+    :param token: User's token.
+    :type token: str
+    :param db: Database session.
+    :type db: Session
+    :return: Not return result.
+    """
     user.refresh_token = token
     db.commit()
 
 
 async def confirmed_email(email: str, db: Session) -> None:
+    """
+    Set user's e-mail confirmed.
+
+    :param email: User's e-mail
+    :type email: str
+    :param db: Database session.
+    :type db: Session
+    :return: Not return result.
+    """
     user = await get_user_by_email(email, db)
     user.confirmed = True
     db.commit()
 
 
 async def update_avatar(email, url: str, db: Session) -> None:
+    """
+    Update user's avatar
+
+    :param email: User's e-mail.
+    :type email: str
+    :param url: User avatar link.
+    :type url: str
+    :param db: Database session.
+    :type db: Session
+    :return: Updated user.
+    :rtype: User
+
+    """
     user = await get_user_by_email(email, db)
     user.avatar = url
     db.commit()
